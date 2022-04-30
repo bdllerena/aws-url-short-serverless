a0957b50d809434e0d6b591e5e31f698b1ff0c6a
bdllerena <bolivar.llerena@kushkipagos.com>
2022-04-30 18:47:35 -0500

    url-shortener project

--- handler.ts
+++ handler.ts
@@ -1,18 +1,13 @@
 import { Handler } from 'aws-lambda';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as generator } from 'uuid';
 import * as AWS from 'aws-sdk';
 
-AWS.config.region = 'us-east-1';
 const s3 = new AWS.S3();
 const dynamodb = new AWS.DynamoDB();
 
-
-const domain: string = "http://cmcloudlab544.info/";
-const application: string = "url_shortner";
-
 let shortName = function(counter: number): string{
-    const base62: string = uuidv4();
-    const base62Length = base62.length;
+    const base62: string = generator();
+    const base62Length: number = base62.length;
     let shortName: string = '';
 
     while (counter) {
@@ -24,13 +19,20 @@ let shortName = function(counter: number): string{
     return shortName;
 }
 
+let addHttpPrefix = function (url:string): string{
+  if (!/^(f|ht)tps?:\/\//i.test(url)) {
+      url = "http://" + url;
+  }
+  return url;
+}
+
 export const short: Handler = (event: any, context: any) => {
 
     let body: any = JSON.parse(event.body)
     let dynamoParams: any = {
         Key: {
             'name': {
-                S: application
+                S: `${process.env.APPLICATION_NAME}`,
             }
         },
         TableName: `${process.env.DYNAMO_ATOMIC}`,
@@ -44,20 +46,19 @@ export const short: Handler = (event: any, context: any) => {
     dynamodb.updateItem(dynamoParams, function (err, data) {
         if (err) context.fail(err, err.stack);
         else {
-            var s3Params = {
-                Bucket: `${process.env.S3_BUCKET}`,
+            let s3Params = {
+                Bucket: `${process.env.S3_NAME}`,
                 Key: shortName(Number(data.Attributes.counterValue.N)),
                 ACL: 'public-read',
                 Body: '',
                 ContentType: 'text/html',
-                WebsiteRedirectLocation: body.url
+                WebsiteRedirectLocation: addHttpPrefix(body.url)
             };
-
-            // put our zero-length object to our S3 bucket
-            s3.putObject(s3Params, function (err, data) {
+            
+            s3.putObject(s3Params, function (err) {
                 if (err) context.fail(err, err.stack);
                 else
-                    context.succeed({ "shortUrl": domain + s3Params.Key });
+                    context.succeed({ "shortUrl": `${process.env.DOMAIN}` + s3Params.Key });
             });
 
         }
